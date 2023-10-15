1186611ac4b6afab08f3bba5509c379e0a30af9d
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2023-10-15 14:55:00 -0500

    Envio de correo electronico con el código 2FA desde el microservicio de seguridad

--- Controllers/NotificacionesController.cs
+++ Controllers/NotificacionesController.cs
@@ -44,8 +44,8 @@ public class NotificacionesController : ControllerBase
         SendGridMessage msg = this.CrearMensajeBase(datos);
         msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
         msg.SetTemplateData(new{
-            name=datos.nombreDestino,
-            message="Esta es su nueva clave... por favor, no la comparta."
+            name = datos.nombreDestino,
+            message = "Esta es su nueva clave... por favor, no la comparta."
         });
         var response = await client.SendEmailAsync(msg);
         if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
@@ -57,11 +57,36 @@ public class NotificacionesController : ControllerBase
             return BadRequest("error al enviar el correo a la direccion" + datos.correoDestino);
         }
     }
+
+    [Route("enviar-correo-2fa")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreo2fa(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("TWOFA_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new{
+            nombre = datos.nombreDestino,
+            mensaje = datos.contenidoCorreo,
+            asunto = datos.asuntoCorreo
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("correo enviado a la direccion: " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("error al enviar el correo a la direccion" + datos.correoDestino);
+        }
+    }
+
     private SendGridMessage CrearMensajeBase(ModeloCorreo datos) {
         var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
         var subject = "Sending with SendGrid is Fun";
         var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
-        var plainTextContent = "plainTextContent";
+        var plainTextContent = datos.contenidoCorreo;
         var htmlContent = datos.contenidoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         return msg;
